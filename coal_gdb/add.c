f37e1b14e7e5202c4016b06661c39577d4621f65
DeathNet123 <arslan.computer2002@gmail.com>
2022-02-16 13:22:08 +0500

    COAL_GDB

---
+++ coal_gdb/add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a, b;
+    printf("Enter Number 1: ");
+    scanf("%d", &a);
+    printf("Enter Number 2: ");
+    scanf("%d", &b);
+    printf("The Result: ");
+    printf("%d", a+b);
+    return 0;
+}
