5a9992052dd6f8d434bd41fcd2a3fb9e60e952a5
DeathNet123 <arslan.computer2002@gmail.com>
2022-03-23 12:25:57 +0500

    removed the garbage

--- coal_gdb/loop.py
+++
@@ -1,4 +0,0 @@
-idx = 0
-while True:
-    print(idx)
-    idx + 1
--- coal_gdb/newf/fork.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-int main(int argc, char *argv[])
-{ 
-    fork() && fork() && fork();
-    fprintf(stdout, "PUCIT\n");
-    exit(0);
-}
