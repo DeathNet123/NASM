148cb2e9ac46483ac4600710a990f12ba3897b83
DeathNet123 <arslan.computer2002@gmail.com>
2022-03-29 01:11:54 +0500

    Added the percentage feature and now can multiple args but still it does not print the header for each file..

--- more_command/more.c
+++ more_command/more.c
@@ -7,7 +7,8 @@
 //Can't intialize the static variables with non-constant elements
 static  int LINES;
 static int COLS;
-void print_stuff(FILE *printer);
+void print_stuff(FILE *printer, int file_overall);
+int count_lines(FILE *f);
 int main(int argc, char *argv[])
 {
     //Non-Canonical Mode input
@@ -22,32 +23,40 @@ int main(int argc, char *argv[])
     }
     else if(argc == 1 && system("test -t 0")) //if stdin is redirected
     {
-        print_stuff(stdin);
+        print_stuff(stdin, count_lines(stdin));
     }
     else //if stdin is not redirected...
     {
-        print_stuff(fopen(argv[1], "r"));
+        for(int idx = 1; idx < argc; idx++)
+        {
+            FILE *opener = fopen(argv[idx], "r");
+            print_stuff(opener, count_lines(opener));
+        }
     }
     return 0;
 }
-void print_stuff(FILE *printer)
+void print_stuff(FILE *printer, int file_overall)
 {
+    int lines = 1;
     FILE *command = fopen("/dev/tty", "r");//this is for taking the input from the user...
     char buffer[COLS];
     int idx = 0;
     while(fgets(buffer, COLS, printer) != NULL)
     {
+        //printf("LINE: %d: ", lines);
         fputs(buffer, stdout);
         idx++;
+        lines++;
         if(idx == LINES-1)
         {
-            printf("\e[7m\033[1m --MORE--\e[m");
+            printf("\e[7m\033[1m --MORE%0.f%--\e[m", (float) lines / file_overall * 100);
             switch(getc(command))
             {
                 case '\n':
                     idx--;
                     break;
                 case 'q':
+                printf("\n\e[1A\033[2K \033[1G");
                     exit(0);
                 case ' ':
                     printf("\n");//this line will add the newline when space bar is pressed so that line 59 can erase the --MORE--
@@ -60,4 +69,16 @@ void print_stuff(FILE *printer)
         }
     }
     fclose(printer);
+}
+int count_lines(FILE* f)
+{
+    int file_overall = 0;
+    char buffer[LINES];
+    while (fgets(buffer, LINES, f))
+    {
+
+        file_overall++;
+    }
+    rewind(f);
+    return file_overall;
 }
