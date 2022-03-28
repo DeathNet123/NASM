e8752ccd56370a9a0a2e948f3abbbc8315f2ca90
DeathNet123 <arslan.computer2002@gmail.com>
2022-03-29 00:03:12 +0500

    added the quit option and space bar option and one line option

--- more_command/more.c
+++ more_command/more.c
@@ -8,13 +8,14 @@
 static  int LINES;
 static int COLS;
 void print_stuff(FILE *printer);
-int command_handle(FILE *tty);
 int main(int argc, char *argv[])
 {
+    //Non-Canonical Mode input
+    system("stty -icanon");
     //Placing the values of LINES AND COLUMNS into a variables..
     LINES = atoi(getenv("LINES"));//since getenv returns char* we converting them into int;
     COLS = atoi(getenv("COLUMNS"));
-    //Did this to check weather the I/O was redirected or not -t 0 test weather fd 0 is connect to terminal or not
+    //Did this to check weather the I/O was redirected or not -t 0 test weather fd 0 is connected to terminal a.k.a tty or not
     if(argc == 1 && !system("test -t 0"))//if stdin is not redirected
     {
         printf("more: bad usage\nTry 'more --help' for more information.\n");
@@ -33,31 +34,30 @@ void print_stuff(FILE *printer)
 {
     FILE *command = fopen("/dev/tty", "r");//this is for taking the input from the user...
     char buffer[COLS];
-    int idx = 1, choose;
+    int idx = 0;
     while(fgets(buffer, COLS, printer) != NULL)
     {
         fputs(buffer, stdout);
         idx++;
-        if(idx == LINES-2)
+        if(idx == LINES-1)
         {
             printf("\e[7m\033[1m --MORE--\e[m");
-            choose = command_handle(command);
-            printf("\e[1A\033[2K \033[0G");
+            switch(getc(command))
+            {
+                case '\n':
+                    idx--;
+                    break;
+                case 'q':
+                    exit(0);
+                case ' ':
+                    printf("\n");//this line will add the newline when space bar is pressed so that line 59 can erase the --MORE--
+                    idx = 0;
+                    break;
+                default:
+                    break;
+            }
+            printf("\e[1A\033[2K \033[1G");
         }
-        switch(choose)
-        {
-            case 0:
-                idx--;
-                break;
-            default:
-                break;
-        }
-    
     }
     fclose(printer);
-}
-int command_handle(FILE *tty)
-{
-    getc(tty);
-    return 0;
 }
