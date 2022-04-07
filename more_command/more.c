db3de9b79418172106dc0ca807041de6b3e94948
DeathNet123 <arslan.computer2002@gmail.com>
2022-04-07 16:30:27 +0500

    added the vim feature but still did not work with I/O redirection

--- more_command/more.c
+++ more_command/more.c
@@ -2,37 +2,48 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include<unistd.h>
-
+//#define DEBUG
 //Can't intialize the static variables with non-constant elements
 static  int LINES;
 static int COLS;
+void handle_editor(FILE *fp);
 void print_stuff(FILE *printer, int file_overall);
 int count_lines(FILE *f);
 int main(int argc, char *argv[])
-{
+{ 
     //Non-Canonical Mode input
     system("stty -icanon");
+    //system("stty -echo");
     //Placing the values of LINES AND COLUMNS into a variables..
     LINES = atoi(getenv("LINES"));//since getenv returns char* we converting them into int;
     COLS = atoi(getenv("COLUMNS"));
+    #ifdef DEBUG
+    handle_editor(/*fopen(argv[1], "r")*/stdin);
+    #endif
     //Did this to check weather the I/O was redirected or not -t 0 test weather fd 0 is connected to terminal a.k.a tty or not
     if(argc == 1 && !system("test -t 0"))//if stdin is not redirected
     {
         printf("more: bad usage\nTry 'more --help' for more information.\n");
     }
-    else if(argc == 1 && system("test -t 0")) //if stdin is redirected
+    else if(system("test -t 0")) //if stdin is redirected
     {
         print_stuff(stdin, count_lines(stdin));
     }
-    else //if stdin is not redirected...
+    for(int idx = 1; idx < argc; idx++)
     {
-        for(int idx = 1; idx < argc; idx++)
+        if(argc > 2)
         {
-            FILE *opener = fopen(argv[idx], "r");
-            print_stuff(opener, count_lines(opener));
+            printf("::::::::::::::\n");
+            printf("%s\n", argv[idx]);
+            printf("::::::::::::::\n");
         }
+        FILE *opener = fopen(argv[idx], "r");
+        print_stuff(opener, count_lines(opener));
     }
+    system("stty icanon");
     return 0;
 }
 void print_stuff(FILE *printer, int file_overall)
@@ -47,7 +58,7 @@ void print_stuff(FILE *printer, int file_overall)
         fputs(buffer, stdout);
         idx++;
         lines++;
-        if(idx == LINES-1)
+        if(idx >= LINES-1)
         {
             printf("\e[7m\033[1m --MORE%0.f%--\e[m", (float) lines / file_overall * 100);
             switch(getc(command))
@@ -62,6 +73,10 @@ void print_stuff(FILE *printer, int file_overall)
                     printf("\n");//this line will add the newline when space bar is pressed so that line 59 can erase the --MORE--
                     idx = 0;
                     break;
+                case 'v':
+                    handle_editor(printer);
+                    printf("\n");
+                    break;
                 default:
                     break;
             }
@@ -72,13 +87,31 @@ void print_stuff(FILE *printer, int file_overall)
 }
 int count_lines(FILE* f)
 {
-    int file_overall = 0;
-    char buffer[LINES];
-    while (fgets(buffer, LINES, f))
+    int file_overall = 1;
+    char buffer[COLS];
+    while (fgets(buffer, COLS, f))
     {
-
         file_overall++;
     }
     rewind(f);
     return file_overall;
+}
+void handle_editor(FILE *fp)
+{
+    char filename[COLS];
+    char proclink[COLS];
+    int real_id = getpid();
+    int fd = fileno(fp);
+    sprintf(proclink, "/proc/%d/fd/%d", real_id, fd);
+    int size = readlink(proclink, filename, COLS);
+    filename[size] = '\0';
+    int pid = fork();
+    if(pid == 0)
+    {
+        char *editor = "vim";
+        char *args[] = {editor, filename, NULL};
+        execvp(editor, args);
+    }
+    wait(NULL);
+    return;
 }
---
+++ more_command/mymore.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source setting.sh
+./a.out $1
