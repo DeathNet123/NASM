523eaac61eb12a4f172a5221a5200853bc0265d2
DeathNet123 <arslan.computer2002@gmail.com>
2022-04-12 23:40:23 +0500

    ls command short version

--- ls_command/ls_command.c
+++ ls_command/ls_command.c
@@ -12,22 +12,27 @@ static int COLS;
 
 int main(int argc, char *argv[])
 {
-    struct dirent *entry;
+    struct dirent **namelist;
     struct stat statbuf;
     LINES = atoi(getenv("LINES"));
     COLS = atoi(getenv("COLUMNS"));
 
-    struct dirent **namelist;
-    int n;
-    n = scandir(argv[1], &namelist, NULL, alphasort);
-    if (n < 0)
+    int count_entries = 0;
+    count_entries = scandir(argv[1], &namelist, NULL, alphasort); // it will scandir and place the read structure in my namelist
+    /*
+    int scandir(const char *dirp, struct dirent ***namelist,
+              int (*filter)(const struct dirent *),
+              int (*compar)(const struct dirent **, const struct dirent **));
+    here the comparison function is alphasort and Filter is pointed towards NULL
+    */
+    if (count_entries < 0)
         perror("scandir");
     else 
     {
-    while (n--) 
+    while (count_entries--) 
         {
-            printf("%s\n", namelist[n]->d_name);
-            free(namelist[n]);
+            printf("%s\n", namelist[count_entries]->d_name);
+            free(namelist[count_entries]);
         }
         free(namelist);
     }
