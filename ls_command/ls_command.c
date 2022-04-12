01187826dd819ae0dea3826d941ba08d01946ec1
DeathNet123 <arslan.computer2002@gmail.com>
2022-04-13 03:10:50 +0500

    changed the structure a little bit to work on -R feature i might have to write down my .h files maybe

--- ls_command/ls_command.c
+++ ls_command/ls_command.c
@@ -5,36 +5,81 @@
 #include <sys/types.h>
 #include<dirent.h>
 #include <sys/stat.h>
+#include <stdbool.h>
 #include <time.h>
 
 static int LINES;
 static int COLS;
+static struct dirent **namelist; //for storing the entries in directory
+static struct stat *info; //for storing the details of each entry
+extern int errorno;
 
+void populate_data(const char *dir_name);
+void free_mem(int mem_size);
 int main(int argc, char *argv[])
 {
-    struct dirent **namelist;
-    struct stat statbuf;
     LINES = atoi(getenv("LINES"));
     COLS = atoi(getenv("COLUMNS"));
-
-    int count_entries = 0;
-    count_entries = scandir(argv[1], &namelist, NULL, alphasort); // it will scandir and place the read structure in my namelist
-    /*
-    int scandir(const char *dirp, struct dirent ***namelist,
-              int (*filter)(const struct dirent *),
-              int (*compar)(const struct dirent **, const struct dirent **));
-    here the comparison function is alphasort and Filter is pointed towards NULL
-    */
-    if (count_entries < 0)
-        perror("scandir");
-    else 
+    if(argc > 1)
     {
-    while (count_entries--) 
+        for(int idx = 1; idx < argc; idx++)
         {
-            printf("%s\n", namelist[count_entries]->d_name);
-            free(namelist[count_entries]);
+            populate_data(argv[idx]);
         }
-        free(namelist);
+    }
+    else
+    {
+        int count = populate_data(".");
+        free_mem(count);
     }
     return 0;
 }
+int populate_data(const char *dir_name)
+{
+    struct stat *info;//will hold the information of the file...
+    struct dirent **namelist;//will hold the entries of the directory..
+    //reading the entries
+    int count_entries = 0; // number of entries in the directory..
+    count_entries = scandir(dir_name, &namelist, NULL, alphasort); // it will scandir and place the read structure in my namelist
+    /*
+    int scandir(const char *dirp, struct dirent **namelist,
+            int (*filter)(const struct dirent *),
+            int (*compar)(const struct dirent **, const struct dirent **));
+    here the comparison function is alphasort and Filter is pointed towards NULL
+    scandir allocate memory by itself but have to deallocate it
+    */
+    if(count_entries < 0)
+    {
+        perror("scandir");
+    }
+    else
+    {
+        info = malloc(count_entries * sizeof(struct stat));
+        for(int idx = 0; idx < count_entries; idx++)
+        {
+            int status = lstat(namelist[idx]->d_name, &info[idx]);
+            if(status == -1)
+                perror("stat failed");
+            printf("mode: %o\n", info->st_mode);
+            printf("link count: %ld\n", info->st_nlink);
+            printf("user: %d\n", info->st_uid);
+            printf("group: %d\n", info->st_gid);
+            printf("size: %ld\n", info->st_size);
+            printf("modtime: %ld\n", info->st_mtime);
+            printf("name: %s\n", namelist[idx]->d_name);
+            printf("------------------------------------\n");
+        }
+    }
+    free_mem(count_entries, info, namelist);
+    return;
+}
+void free_mem(int mem_size, struct stat *info, struct dirent **namelist)
+{
+    for(int idx = 0; idx < mem_size; idx++)
+    {
+        free(namelist[idx]);
+    }
+        free(namelist);
+        free(info);
+    return;
+}       
