b686cb4df4d5fb72defccaef1b524fffc23fd187
DeathNet123 <arslan.computer2002@gmail.com>
2022-06-09 18:51:51 +0500

    game

--- socket_programming/web-server/webserver.c
+++ socket_programming/web-server/webserver.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(54154); //alternative port to 80
-    inet_aton("192.168.18.24", &server_address.sin_addr);
+    inet_aton("172.22.174.232", &server_address.sin_addr);
     memset(&server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
     bind(server_socket_fd, (struct sockaddr *) &server_address, sizeof(server_address));
     listen(server_socket_fd, 10);
@@ -30,9 +30,12 @@ int main(int argc, char *argv[])
         int c_len = sizeof(client_address);
         client_socket_fd = accept(server_socket_fd, (struct sockaddr *) &client_address, &c_len);
         int rv = 0;
-        char buf[1024];
+        char buf[5000];
         while(rv = read(client_socket_fd, buf, rv))
+        {
             write(1, buf, rv);
+        }
+        write(client_socket_fd, "hello this is the test complete", 32);
         printf("Bye bye the test has been completed\n");
         close(client_socket_fd);
     }
