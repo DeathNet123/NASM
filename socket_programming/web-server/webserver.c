1b788952c09aa4624ca280555d5f13461f3d81d2
DeathNet123 <arslan.computer2002@gmail.com>
2022-06-09 18:36:20 +0500

    done some changes in webserver

--- socket_programming/web-server/webserver.c
+++ socket_programming/web-server/webserver.c
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
     server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(8080); //alternative port to 80
-    inet_aton("192.168.198.128", &server_address.sin_addr);
+    inet_aton("192.168.18.24", &server_address.sin_addr);
     memset(&server_address.sin_zero, '\0', sizeof(server_address.sin_zero));
     bind(server_socket_fd, (struct sockaddr *) &server_address, sizeof(server_address));
     listen(server_socket_fd, 10);
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
         int rv = 0;
         char buf[1024];
         while(rv = read(client_socket_fd, buf, rv))
-            write(0, buf, rv);
+            write(1, buf, rv);
         printf("Bye bye the test has been completed");
         close(client_socket_fd);
     }
